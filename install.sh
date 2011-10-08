6ca725d546d1b1c58f9e63db2608a4ccbab146a7
cenan ozen <cenan.ozen@gmail.com>
2011-10-08 12:08:25 +0300

    changed git user name, added sqliterc

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@
 ln -fs $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
 ln -fs $HOME/dotfiles/.guile $HOME/.guile
 ln -fs $HOME/dotfiles/.gitconfig $HOME/.gitconfig
+ln -fs $HOME/dotfiles/.sqliterc $HOME/.sqliterc
 
