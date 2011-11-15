a2e6b4f7afb047774de604ca29ced9046c8708f8
cenan ozen <cenan.ozen@gmail.com>
2011-11-15 20:41:30 +0200

    added conky output to tmux statusbar

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 ln -fs $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -fs $HOME/dotfiles/.conkytmux $HOME/.conkytmux
 ln -fs $HOME/dotfiles/.guile $HOME/.guile
 ln -fs $HOME/dotfiles/.gitconfig $HOME/.gitconfig
 ln -fs $HOME/dotfiles/.sqliterc $HOME/.sqliterc
