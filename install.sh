33d3e6934f863ff04aff3d72d7474e5fca0f3ac3
cenan ozen <cenan.ozen@gmail.com>
2011-12-02 12:56:23 +0200

    Added hgrc

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ ln -fs $SCRIPT_DIR/tmux.conf $HOME/.tmux.conf
 ln -fs $SCRIPT_DIR/conkytmux $HOME/.conkytmux
 ln -fs $SCRIPT_DIR/guile $HOME/.guile
 ln -fs $SCRIPT_DIR/gitconfig $HOME/.gitconfig
+ln -fs $SCRIPT_DIR/hgrc $HOME/.hgrc
 ln -fs $SCRIPT_DIR/sqliterc $HOME/.sqliterc
 ln -fs $SCRIPT_DIR/dotjs $HOME/.js
 ln -fs $SCRIPT_DIR/zshrc $HOME/.zshrc
