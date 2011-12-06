a7882a8c10d2b8667d633e96017a356c65afb244
cenan ozen <cenan.ozen@gmail.com>
2011-12-06 11:33:33 +0200

    change awesome tag names

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -49,7 +49,7 @@ layouts =
 tags = {}
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
-    tags[s] = awful.tag({ "default", "browse", "code", "term", "last" }, s, layouts[1])
+    tags[s] = awful.tag({ "1", "2", "3", "4", "5" }, s, layouts[1])
 end
 -- }}}
 
