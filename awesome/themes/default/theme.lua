347d0e44c749a23ce07c51559a514da7078e16d8
cenan ozen <cenan.ozen@gmail.com>
2011-12-06 11:52:23 +0200

    Change default wallpaper

--- awesome/themes/default/theme.lua
+++ awesome/themes/default/theme.lua
@@ -75,7 +75,7 @@ theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/defau
 
 -- You can use your own command to set your wallpaper
 -- theme.wallpaper_cmd = { "awsetbg /usr/share/awesome/themes/default/background.png" }
-theme.wallpaper_cmd = { "awsetbg /home/cenan/Pictures/wallbase.cc/wallpaper-777463.jpg" }
+theme.wallpaper_cmd = { "awsetbg /home/cenan/Pictures/out.png" }
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
