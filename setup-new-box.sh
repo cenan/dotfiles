697847c0965c69cfdbd3b176793bff294a021bac
cenan ozen <cenan.ozen@gmail.com>
2011-12-07 12:42:29 +0200

    Added setup-new-box script

---
+++ setup-new-box.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt-get install git-core
+
+sudo apt-get install mercurial
+sudo apt-get install tortoisehg
+
+sudo apt-get install exuberant-ctags
+sudo apt-get install vim-gnome
+
+sudo apt-get install tmux
+
+sudo apt-get install moc
+
+sudo apt-get install awesome
+
