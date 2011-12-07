9cbb31362e01189c773c2815573de7c0b3b3fa21
cenan ozen <cenan.ozen@gmail.com>
2011-12-08 00:33:01 +0200

    setup-new-box

--- setup-new-box.sh
+++ setup-new-box.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+sudo apt-get install curl
+sudo-apt-get install openssh-server
+sudo apt-get install moc
+sudo apt-get install conky
+
 sudo apt-get install git-core
+sudo apt-get install tig
+sudo apt-get install git-gui
 
 sudo apt-get install mercurial
 sudo apt-get install tortoisehg
@@ -9,10 +16,10 @@ sudo apt-get install exuberant-ctags
 sudo apt-get install vim-gnome
 
 sudo apt-get install tmux
-
-sudo apt-get install moc
-
 sudo apt-get install awesome
+
+sudo apt-get install sqlite3
+sudo apt-get install python-mysqldb
 
 su -c "echo 'Dir::Ignore-Files-Silently:: \"(.save)$\";' > /etc/apt/apt.conf.d/99ignoresave"
 
