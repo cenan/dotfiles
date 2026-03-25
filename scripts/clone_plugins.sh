852ba80e030a8d58d4c6d0c82123dc3c94d21571
cenan <cenan.ozen@gmail.com>
2026-03-25 08:56:55 +0300

    Updates clone plugins script

--- scripts/clone_plugins.sh
+++ scripts/clone_plugins.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+git clone https://github.com/neovim/nvim-lspconfig ~/.config/nvim/pack/nvim/start/nvim-lspconfig
 git clone https://github.com/nvim-mini/mini.nvim ~/.config/nvim/pack/nvim/start/mini.nvim
 git clone https://www.github.com/nvim-lua/plenary.nvim ~/.config/nvim/pack/nvim/start/plenary.nvim
 git clone --branch v19.6.0 https://www.github.com/olimorris/codecompanion.nvim ~/.config/nvim/pack/nvim/start/codecompanion.nvim
